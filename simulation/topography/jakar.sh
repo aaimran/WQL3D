2c8b4c77a35b06de0a3baf002fe861e2d73c1045
aimran <aimran@miners.utep.edu>
2026-04-23 13:46:28 -0600

    topography

--- simulation/topography/jakar.sh
+++ simulation/topography/jakar.sh
@@ -5,9 +5,17 @@ module load compiler-rt/2024.0.0 ifort/2024.0.0 mpi/2021.13
 
 mkdir -p ./output
 
-fname="topo_1tpx_100m"
+fname="topo_1up_100m"
 mpirun -np $nprocs ../../build/./waveqlab3d ./input/${fname}.in | tee ./output/${fname}.out
 
+fname="topo_2up_100m"
+mpirun -np $nprocs ../../build/./waveqlab3d ./input/${fname}.in | tee ./output/${fname}.out
 
-#fname="topo_1tp_100m"
-#mpirun -np $nprocs ../../build/./waveqlab3d ./input/${fname}.in | tee ./output/${fname}.out
+fname="topo_3up_100m"
+mpirun -np $nprocs ../../build/./waveqlab3d ./input/${fname}.in | tee ./output/${fname}.out
+
+fname="topo_4up_100m"
+mpirun -np $nprocs ../../build/./waveqlab3d ./input/${fname}.in | tee ./output/${fname}.out
+
+fname="topo_5up_100m"
+mpirun -np $nprocs ../../build/./waveqlab3d ./input/${fname}.in | tee ./output/${fname}.out
